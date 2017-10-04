82f9c62ac321a7d8b0a3f3ad6b84fd11b2da55b2
Benknightdark <moon791017@gmail.com>
2017-10-04 13:36:13 +0800

    add download and upload file

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
-import {HttpResponse, HttpEventType,  HttpRequest,   HttpHeaders,    HttpClient} from '@angular/common/http';
+import { HttpResponse, HttpEventType, HttpRequest, HttpHeaders, HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 import 'rxjs/add/operator/map';
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/shareReplay';
 import * as _ from 'lodash';
+import { DomSanitizer } from '@angular/platform-browser';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -11,11 +12,29 @@ import * as _ from 'lodash';
 })
 export class AppComponent {
 
-  constructor(private http: HttpClient) { }
-  SpecData: Observable<any>
+  constructor(private http: HttpClient, private _DomSanitizationService: DomSanitizer) { }
+  SpecData: Observable<any>;
   show = false;
+  imgsource
+
+
 
   ngOnInit() {
+
+    this.http.get('/apiAzureBlob/' + '2f9725e3-7b28-4035-aa89-7beb54f8cb20', { responseType: 'arraybuffer' })
+
+      .subscribe(d => {
+        console.log(d)
+        // Prep the response for Base64 encoding
+        const uInt8Array = new Uint8Array(d);
+        let i = uInt8Array.length;
+        const binaryString = new Array(i);
+        while ( i--) {
+          binaryString[i] = String.fromCharCode(uInt8Array[i]);
+        }
+        const data = binaryString.join('');
+        this.imgsource = window.btoa(data)
+      });
 
     this.http.post('/api/Account', {
       'WorkID': '',
@@ -36,18 +55,18 @@ export class AppComponent {
     }).map(a => _.values(a[0])).shareReplay();
   }
   fileChange(event) {
-console.log()
+    console.log()
 
     const formData: FormData = new FormData();
     const fileList: FileList = event.target.files;
 
-      const file: File = fileList[0];
+    const file: File = fileList[0];
     formData.append('image', file, file.name);
     const headers = new HttpHeaders();
     const req = new HttpRequest('POST',
-    '/apiAzureBlob', formData, {
-     reportProgress: true,
-   });
+      '/apiAzureBlob', formData, {
+        reportProgress: true,
+      });
     this.http.request(req).subscribe(evuplent => {
       if (evuplent.type === HttpEventType.UploadProgress) {
         const percentDone = Math.round(100 * evuplent.loaded / evuplent.total);
@@ -56,8 +75,10 @@ console.log()
       } else if (evuplent instanceof HttpResponse) {
         console.log('File is completely uploaded!');
         console.log(evuplent);
+        //console.log(evuplent['body']);
       }
     });
+
   }
 
 }
